a7e83b3a736d9a9af6e5984d9f58c95408b0704a
stdraganova <svetlana.draganova98@gmail.com>
2026-06-19 17:23:18 +0300

    feat: initial commit

---
+++ src/main/java/com/rewe/studentstrackingsystem/StudentsTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rewe.studentstrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsTrackingSystemApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(StudentsTrackingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rewe/studentstrackingsystem/StudentsTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewe.studentstrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsTrackingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
