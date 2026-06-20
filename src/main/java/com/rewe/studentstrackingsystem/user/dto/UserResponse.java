393ef2295b77500e9deab8e10e0a897581644214
stdraganova <svetlana.draganova98@gmail.com>
2026-06-20 20:47:41 +0300

    fix:removed unused imports

--- src/main/java/com/rewe/studentstrackingsystem/user/dto/UserResponse.java
+++ src/main/java/com/rewe/studentstrackingsystem/user/dto/UserResponse.java
@@ -1,7 +1,5 @@
 package com.rewe.studentstrackingsystem.user.dto;
 
-import com.rewe.studentstrackingsystem.user.entity.Role;
-
 public record UserResponse(
         String username,
         String firstName,
