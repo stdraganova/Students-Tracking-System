928790400f06ef321df981dcb0e79f3ad487aa72
stdraganova <svetlana.draganova98@gmail.com>
2026-06-21 22:03:45 +0300

    fix: fixed unmapped fields

--- src/main/java/com/rewe/studentstrackingsystem/grade/mapper/GradeMapper.java
+++ src/main/java/com/rewe/studentstrackingsystem/grade/mapper/GradeMapper.java
@@ -4,10 +4,13 @@ import com.rewe.studentstrackingsystem.grade.dto.GradeRequest;
 import com.rewe.studentstrackingsystem.grade.dto.GradeResponse;
 import com.rewe.studentstrackingsystem.grade.entity.Grade;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GradeMapper {
 
+    @Mapping(target = "student", ignore = true)
+    @Mapping(target = "course", ignore = true)
     Grade toEntity(GradeRequest gradeRequest);
 
     GradeResponse toResponse(Grade grade);
