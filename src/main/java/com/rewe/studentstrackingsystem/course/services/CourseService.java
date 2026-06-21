c4d78ce6692b1d27df0b142a0a0b1f9997f81450
stdraganova <svetlana.draganova98@gmail.com>
2026-06-21 12:23:28 +0300

    feat: Added propper create course functionality

--- src/main/java/com/rewe/studentstrackingsystem/course/services/CourseService.java
+++ src/main/java/com/rewe/studentstrackingsystem/course/services/CourseService.java
@@ -4,6 +4,7 @@ import com.rewe.studentstrackingsystem.course.dto.CourseRequest;
 import com.rewe.studentstrackingsystem.course.dto.CourseResponse;
 import com.rewe.studentstrackingsystem.course.mapper.CourseMapper;
 import com.rewe.studentstrackingsystem.course.repository.CourseRepository;
+import com.rewe.studentstrackingsystem.teacher.repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,19 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class CourseService {
     private final CourseRepository courseRepository;
+    private final TeacherRepository teacherRepository;
     private final CourseMapper courseMapper;
 
-    public CourseResponse save(CourseRequest courseRequest) {
-        var savedCourse = courseRepository.save(courseMapper.toEntity(courseRequest));
+    public CourseResponse create(CourseRequest courseRequest) {
+        var newCourse = courseMapper.toEntity(courseRequest);
+        var teacher = teacherRepository.findById(courseRequest.teacherId())
+                .orElseThrow(() -> new IllegalArgumentException("Teacher not found with id: " + courseRequest.teacherId()));
+        newCourse.setTeacher(teacher);
+        teacher.getCourses().add(newCourse);
+
+        teacherRepository.save(teacher);
+        var savedCourse = courseRepository.save(newCourse);
+
         return courseMapper.toResponse(savedCourse);
     }
 
