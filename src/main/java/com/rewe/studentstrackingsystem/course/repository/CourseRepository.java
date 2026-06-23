4fdfee0a75336162fdb34b62fef61a0e935926e1
stdraganova <svetlana.draganova98@gmail.com>
2026-06-23 09:16:49 +0300

    fix: removed unused import

--- src/main/java/com/rewe/studentstrackingsystem/course/repository/CourseRepository.java
+++ src/main/java/com/rewe/studentstrackingsystem/course/repository/CourseRepository.java
@@ -4,7 +4,6 @@ import com.rewe.studentstrackingsystem.course.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
